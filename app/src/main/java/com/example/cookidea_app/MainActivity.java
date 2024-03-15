e0499a30d04333df2149633fc4500b99f1d3cdf0
Anais <anais.casamento@itsrizzoli.it>
2024-03-15 11:50:33 +0100

    primo commit

---
+++ app/src/main/java/com/example/cookidea_app/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.cookidea_app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.getOrCreateBadge(R.id.home_page).setVisible(true);
+        bottomNavigationView.getOrCreateBadge(R.id.search).setVisible(true);
+        bottomNavigationView.getOrCreateBadge(R.id.menu).setVisible(true);
+        
+
+
+
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+
+            }
+        });
+
+    }
+
+
+
+}
