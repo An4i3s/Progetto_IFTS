4405edf191c3abe16c8f003279d51aede5389251
Anais <anais.casamento@itsrizzoli.it>
2024-03-16 13:42:10 +0100

    aggiunto DrwaerNavigationBar

--- app/src/main/java/com/example/cookidea_app/MainActivity.java
+++ app/src/main/java/com/example/cookidea_app/MainActivity.java
@@ -5,26 +5,55 @@ import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //QUI DEVO FARE PROVE COL LAYOUT
+        /* Possibile soluzione
+        * seContentView ( a drawer, il quale include al suo interno activity_main)
+        * Provare a fare andare la prima volta senza cambiamenti
+        * */
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_drawer);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.getOrCreateBadge(R.id.home_page).setVisible(true);
         bottomNavigationView.getOrCreateBadge(R.id.search).setVisible(true);
         bottomNavigationView.getOrCreateBadge(R.id.menu).setVisible(true);
-        
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_camera);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+       // ImportFragment fragment = new ImportFragment();
+       // fragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
 
 
 
@@ -35,8 +64,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        Fragment fragment = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (id == R.id.nav_camera) {
+           // fragment = new ImportFragment();
+        } else if (id == R.id.nav_gallery) {
+           // fragment = new GalleryFragment();
+        }
+
+        //fragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
