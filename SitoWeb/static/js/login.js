7a5e1f0386bf36e4f8a3cb5ec4824b120fd039a4
elKryptos <hans_09@hotmail.it>
2024-03-25 15:06:10 +0100

    carosello funzionante css e js, login css, js

--- SitoWeb/static/js/login.js
+++ SitoWeb/static/js/login.js
@@ -1,4 +1,4 @@
-const wrapper = document.querySelector('.wrapper');
+/** const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
@@ -18,13 +18,34 @@ loginLink.addEventListener('click', ()=> {
   });
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
-    hiddenTab.style.display = 'grid';
+    hiddenTab.style.display = 'flex';
 
 });
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
-    hiddenTab.style.display = 'grid';
+    hiddenTab.style.display = 'flex';
 
+}); **/
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active')
 });
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active')
+});
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup')
+});
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup')
+});
+
+
 
 
