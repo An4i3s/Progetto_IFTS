2f5d3a2052825794339238f43ebde0cba7b5693c
Anaïs <76781733+An4i3s@users.noreply.github.com>
2024-03-27 15:43:49 +0100

    Creato Fragment Profilo

---
+++ AppAndroid/app/src/main/java/com/example/cookidea_app/Fragments/ProfiloFragment.java
@@ -0,0 +1,27 @@
+package com.example.cookidea_app.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.cookidea_app.R;
+
+public class ProfiloFragment extends Fragment {
+
+    public ProfiloFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_profilo, container, false);
+
+        return rootView;
+    }
+}
