4b61ae1c3bdd642dad5f9ae6c5f8f4eb8417cc77
Anaïs <76781733+An4i3s@users.noreply.github.com>
2024-03-28 12:18:57 +0100

    Nasconde inputPassword

--- AppAndroid/app/src/main/java/com/example/cookidea_app/Fragments/LoginFragment.java
+++ AppAndroid/app/src/main/java/com/example/cookidea_app/Fragments/LoginFragment.java
@@ -56,6 +56,7 @@ public class LoginFragment extends Fragment {
 
         username = rootView.findViewById(R.id.usernameEditText);
         password = rootView.findViewById(R.id.passwordEditText);
+        password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         checkBox = rootView.findViewById(R.id.checkbox);
         loginBtn = rootView.findViewById(R.id.signinButton);
 
