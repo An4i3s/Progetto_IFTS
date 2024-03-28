b1de1270334d7e6e6b2dcca11e74c30eeeb8b328
simoiannelli <simone.iannelli@itsrizzoli.it>
2024-03-28 17:32:30 +0100

    aggiunte css e javascript spesa

--- SitoWeb/static/js/spesa.js
+++ SitoWeb/static/js/spesa.js
@@ -1,16 +1,23 @@
-  // Aggiungi un event listener alla lista della spesa
-  document.getElementById('shopping-list').addEventListener('click', function(event) {
-    // Verifica se l'elemento cliccato è un ingrediente (elemento <li>)
-    if (event.target.tagName === 'LI') {
-        // Chiama la funzione per barrare l'ingrediente
-        toggleIngredient(event.target);
-    }
-});
+//   // Aggiungi un event listener alla lista della spesa
+//   document.getElementById('shopping-list').addEventListener('click', function(event) {
+//     // Verifica se l'elemento cliccato è un ingrediente (elemento <li>)
+//     if (event.target.tagName === 'LI') {
+//         // Chiama la funzione per barrare l'ingrediente
+//         toggleIngredient(event.target);
+//     }
+// });
 
 // Funzione per barrare o sbarrare un ingrediente
 function toggleIngredient(ingredient) {
     ingredient.style.textDecoration = (ingredient.style.textDecoration === 'line-through') ? 'none' : 'line-through';
 }
+
+document.querySelectorAll('.toggle-ingrdient').forEach(function(button){
+    button.addEventListener('click', function(){
+        var ingredient = this.parentNode;
+        toggleIngredient(ingredient);
+    });
+});
 
 document.getElementById('clear-checked').addEventListener('click', function(){
     var ingredients = document.querySelectorAll('#shopping-list li');
