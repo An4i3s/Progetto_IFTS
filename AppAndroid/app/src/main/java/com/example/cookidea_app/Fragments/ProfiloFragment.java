a265326cabfead51d17f2f1775271a7eb7e6b3dc
Anaïs <76781733+An4i3s@users.noreply.github.com>
2024-03-29 16:19:20 +0100

    Aggiunto funzionalità modifica cognome e data di nascita in Profilo Fragmnet

--- AppAndroid/app/src/main/java/com/example/cookidea_app/Fragments/ProfiloFragment.java
+++ AppAndroid/app/src/main/java/com/example/cookidea_app/Fragments/ProfiloFragment.java
@@ -1,5 +1,6 @@
 package com.example.cookidea_app.Fragments;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -24,6 +26,9 @@ import com.example.cookidea_app.R;
 
 import org.w3c.dom.Text;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -36,9 +41,16 @@ public class ProfiloFragment extends Fragment {
     TextView cognomeTv;
     TextView dataNascitaTv;
 
+
     EditText editNome;
+    EditText editCognome;
     ImageButton btnNome;
     ImageButton btnConfNome;
+
+    ImageButton btnCognome;
+    ImageButton btnConfCognome;
+
+    ImageButton btnDataNascita;
     SharedPreferences sharedPreferences;
 
     Retrofit retrofit = new Retrofit.Builder().baseUrl(MainActivity.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
@@ -73,10 +85,16 @@ public class ProfiloFragment extends Fragment {
         cognomeTv.setText(user.getSurname());
         dataNascitaTv = rootView.findViewById(R.id.dataNascita);
         dataNascitaTv.setText(user.getDate2());
+
         editNome = rootView.findViewById(R.id.editNome);
+        editCognome = rootView.findViewById(R.id.editCognome);
 
         btnNome = rootView.findViewById(R.id.btnEditNome);
         btnConfNome = rootView.findViewById(R.id.btnConfNome);
+        btnCognome = rootView.findViewById(R.id.btnEditCognome);
+        btnConfCognome = rootView.findViewById(R.id.btnConfCognome);
+        btnDataNascita = rootView.findViewById(R.id.btnDataNascita);
+
 
 
         btnNome.setOnClickListener(new View.OnClickListener() {
@@ -87,6 +105,60 @@ public class ProfiloFragment extends Fragment {
                 btnNome.setVisibility(View.GONE);
                 btnConfNome.setVisibility(View.VISIBLE);
                 editNome.setText(nomeTv.getText().toString());
+            }
+        });
+
+        btnCognome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cognomeTv.setVisibility(View.GONE);
+                editCognome.setVisibility(View.VISIBLE);
+                btnCognome.setVisibility(View.GONE);
+                btnConfCognome.setVisibility(View.VISIBLE);
+                editCognome.setText(cognomeTv.getText().toString());
+            }
+        });
+
+        btnDataNascita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog fragmentDate = new DatePickerDialog(rootView.getContext());
+                fragmentDate.show();
+                fragmentDate.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Calendar mCalendar = Calendar.getInstance();
+                        mCalendar.set(Calendar.YEAR, year);
+                        mCalendar.set(Calendar.MONTH, month);
+                        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        String selectedDate = DateFormat.getDateInstance(DateFormat.SHORT).format(mCalendar.getTime());
+                        dataNascitaTv.setText(selectedDate);
+                    }
+                });
+            }
+        });
+
+
+        // TODO: 29/03/2024 Implemnetare collegamento a API per update dati utenti in DB 
+        btnConfNome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nomeTv.setText(editNome.getText());
+                nomeTv.setVisibility(View.VISIBLE);
+                btnNome.setVisibility(View.VISIBLE);
+                editNome.setVisibility(View.GONE);
+                btnConfNome.setVisibility(View.GONE);
+            }
+        });
+
+        btnConfCognome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cognomeTv.setText(editCognome.getText());
+                cognomeTv.setVisibility(View.VISIBLE);
+                btnCognome.setVisibility(View.VISIBLE);
+                editCognome.setVisibility(View.GONE);
+                btnConfCognome.setVisibility(View.GONE);
             }
         });
 
