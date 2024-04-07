1b4d00e6f979aea53f4f92a488d517e539fb8ae9
pazlin <pazlin14@gmail.com>
2024-04-07 16:54:03 +0200

    Create MealArrayAdapter.java

---
+++ AppAndroid/app/src/main/java/com/example/cookidea_app/Adapters/MealArrayAdapter.java
@@ -0,0 +1,41 @@
+package com.example.cookidea_app.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.cookidea_app.ModelClasses.Meal;
+
+import java.util.List;
+
+public class MealArrayAdapter extends ArrayAdapter<Meal> {
+
+    Context ctx = null;
+
+    public MealArrayAdapter(Context ctx, List<Meal> listMeals) {
+        super(ctx, android.R.layout.simple_spinner_item, listMeals);
+        this.ctx = ctx;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(ctx).inflate(android.R.layout.simple_spinner_item, parent, false);
+        }
+        Meal meal = getItem(position);
+
+        if (meal != null) {
+            ((TextView) convertView.findViewById(android.R.id.text1)).setText(meal.getMealName());
+        }
+
+        return convertView;
+    }
+}
